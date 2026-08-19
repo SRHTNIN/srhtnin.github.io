60f52115faacd5f695f8fbfc65fa7e4d6e1ca53f
Sarahtonin <SRHTNIN@proton.me>
2026-08-19 20:20:01 +0200

    Added content to about me  page.

--- Pages/Sarahtonin.php
+++ Pages/Sarahtonin.php
@@ -16,12 +16,68 @@ $PageTitle = "Sarahtonin";
             <h1>About me</h1>
 
             <p>
-                Hi. There's nothing here yet, sorry.
+                I'm Sarah, also known as Sarahtonin.
+                I spend a lot of my time making things for niche uses,
+                usually because I wanted something to exist and couldn't find quite the right thing already.
             </p>
 
             <p>
-                <a href="/">Back home</a>
+                A lot of what I do revolves around computers, programming,
+                Linux, and generally messing around with technology until it does what I want.
             </p>
+
+            <h2>Skills</h2>
+
+            <p>
+                I work in IT, so I've learned a lot of different stuff in my time.
+                The things I'm best at (in descending order) are:
+            </p>
+
+            <ul>
+                <li>Python</li>
+                <li>Linux</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>PHP</li>
+                <li>JavaScript</li>
+            </ul>
+
+            <p>
+                I prefer modular systems and reusable components.
+                No need to have stuff installed that you won't use anyway.
+                <br>
+                "Quality over quantity" describes my preferences pretty well.
+            </p>
+
+            <h2>Who I am</h2>
+
+            <p>
+                I'm Norwegian, my pronouns are she/her,
+                and I generally spend my free time playing games (<em>not just videogames</em>), programming,
+                using Linux, or drawing pixel-art.
+            </p>
+
+            <p>
+                I'm also asexual, lesbian, and trans. It's quite queer..
+            </p>
+
+            <h3>Socials</h3>
+
+            <p>
+                My username tends to be either <strong>Sarahtonin</strong>
+                or<strong>SRHTNIN</strong> online.
+                I'm named that on Steam, YouTube, and Discord.
+            </p>
+
+            <h3>Misc info</h3>
+
+            <ul>
+                <li>Birthday: 27 November 2008</li>
+                <li>Nationality: Norwegian</li>
+                <li>Gender: Female</li>
+                <li>Pronouns: She/Her</li>
+            </ul>
         </main>
 
         <script src="/Scripts/Script.js"></script>
